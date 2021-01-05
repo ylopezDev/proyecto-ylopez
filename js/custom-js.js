815894b23b808ef6c6bffa9dfe1fd190e9e38672
ylopezDev <68521846+ylopezDev@users.noreply.github.com>
2021-01-05 19:31:04 -0300

    Primera subida de proyecto

---
+++ js/custom-js.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoader', function(event) {
+    document.getElementById('button').addEventListener('click', alerta, false );
+});
+
+function alerta(){
+    alert("¿Estás listo para sumergirte en la aventura?");
+}
+
+
+
+
+
+
+
+
+
+
+
